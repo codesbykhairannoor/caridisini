73f2a4b9d876d99e75ab933a97631db1568e9c1f
Khairan Aja <khairking6@gmail.com>
2026-04-11 00:27:25 +0800

    fix: add missing import for ProductContainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ProductCard from "@/components/ProductCard";
+import ProductContainer from "@/components/ProductContainer";
 import prisma from "@/lib/prisma";
 import Image from "next/image";
 import { Search, TrendingUp, Sparkles, Zap, ShieldCheck } from "lucide-react";
 
 export const revalidate = 0;
+
 
 export default async function Home() {
   const products = await prisma.product.findMany({
